7f770242e9e8f840f3e52da7aefc2ac02add412a
Benson <njugunab655@gmail.com>
2023-08-24 18:05:22 +0300

    Add users api controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,23 @@
+class Api::UsersController < ApplicationController
+  # skip_before_action :verify_authenticity_token
+
+  def register
+    user = User.new(username: params[:username])
+
+    if user.save
+      render json: user, status: :created
+    else
+      render json: { error: user.errors.full_messages.join(', ') }, status: :unprocessable_entity
+    end
+  end
+
+  def login
+    user = User.find_by(username: params[:username])
+
+    if user
+      render json: user
+    else
+      render json: { error: 'User not found' }, status: :not_found
+    end
+  end
+end
