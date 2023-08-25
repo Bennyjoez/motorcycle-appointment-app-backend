7904e0db890264ed5188f503ea704e6f14d23dd5
Benson <njugunab655@gmail.com>
2023-08-25 10:00:46 +0300

    Change validation restrictions

--- app/models/motorcycle.rb
+++ app/models/motorcycle.rb
@@ -1,6 +1,6 @@
 class Motorcycle < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  validates :image, presence: true, uniqueness: true
-  validates :amount, presence: true, uniqueness: true
-  validates :duration, presence: true, uniqueness: true
+  validates :image, presence: true
+  validates :amount, presence: true
+  validates :duration, presence: true
 end
