694457452ef3b06fb87289024ec3f584c5f937b7
Benson <njugunab655@gmail.com>
2023-08-28 15:39:22 +0300

    Add user request specs

--- spec/requests/api/users_spec.rb
+++ spec/requests/api/users_spec.rb
@@ -1,7 +1,56 @@
 require 'rails_helper'
 
-RSpec.describe 'Api::Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "Api::Users", type: :request do
+  let(:user) {create(:user)}
+
+  before do
+    # user.save
+  end
+
+  after do
+    User.destroy_all
+  end
+
+  describe 'POST #register' do
+    context 'with valid attributes' do
+      it 'creates a new user' do
+        post '/api/register', params: { username: 'new_user' }
+        expect(response).to have_http_status(:created)
+        expect(User.count).to eq(1)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not create a user' do
+        post '/api/register', params: { username: 'John' }
+        post '/api/register', params: { username: 'John' }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(User.count).to eq(1)
+      end
+    end
+  end
+
+  describe 'GET #login' do
+    context 'when user exists' do
+      it "returns the user's information" do
+        user = create(:user, username: 'existing_user')
+        get '/api/login', params: { username: 'existing_user' }
+        expect(response).to have_http_status(:ok)
+        expect(response_body['username']).to eq(user.username)
+      end
+    end
+
+    context 'when user does not exist' do
+      it "returns a 'not found' error" do
+        get '/api/login', params: { username: 'non_existent_user' }
+        expect(response).to have_http_status(:not_found)
+        expect(response_body['error']).to eq('User not found')
+      end
+    end
+  end
+
+  # Helper method to parse JSON response body
+  def response_body
+    JSON.parse(response.body)
   end
 end
