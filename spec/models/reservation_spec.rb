008caa6febcb36100a82e8bc879e6d4612e150b4
Harshika Govind <harshika0102@gmail.com>
2023-08-28 21:07:05 +0530

    Fix reservation testing code

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -2,22 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
   describe 'validations' do
-    # ...
+    it { should validate_presence_of(:date) }
+    it { should validate_presence_of(:city) }
+
+    it 'validates case-insensitive uniqueness of motorcycle_id within the scope of date' do
+      existing_reservation = create(:reservation)
+      subject = build(:reservation, motorcycle: existing_reservation.motorcycle, date: existing_reservation.date)
+      expect(subject).not_to be_valid
+      expect(subject.errors[:motorcycle_id]).to include('has already been taken')
+    end
   end
 
   describe 'uniqueness validation' do
     it 'validates uniqueness of motorcycle_id and date' do
       existing_reservation = create(:reservation)
-      puts "Existing motorcycle: #{existing_reservation.motorcycle.inspect}"
-      puts "Existing date: #{existing_reservation.date.inspect}"
-
       subject = build(:reservation, motorcycle: existing_reservation.motorcycle, date: existing_reservation.date)
-      puts "Subject attributes: #{subject.attributes}"
       
-      # Save the subject and check if it's invalid
-      subject.save
-      puts "Subject errors: #{subject.errors.full_messages}"
-      
+      expect(subject).to_not be_valid
       expect(subject.errors[:motorcycle_id]).to include('has already been taken')
     end
   end
