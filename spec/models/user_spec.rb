a67d50155e07f45707aa42df47f7b37d6624ad03
Benson <njugunab655@gmail.com>
2023-08-28 12:57:22 +0300

    Add user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {create(:user)}
+  
+  before do
+    user.save
+  end
+  
+  it "is valid with valid attributes" do
+    expect(user).to be_valid
+  end
+  
+  it "has a unique username" do
+    user2 = build(:user)
+    expect(user2).to_not be_valid
+  end
+
+  it "username cannot be blank" do
+    user = build(:user, username: nil)
+    expect(user).to_not be_valid
+  end
+
+  
 end
