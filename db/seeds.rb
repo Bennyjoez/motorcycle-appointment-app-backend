5fc0019c4cca7fd1137b857cb25ef54cb4c03316
Harshika Govind <harshika0102@gmail.com>
2023-08-31 20:18:05 +0530

    added description in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 motorcycles_data = [
-  { name: 'Harley Davidson', image: 'https://image.kilimall.com/kenya/shop/store/goods/7442/2021/07/7442_06801939004978952_720.jpg.webp', amount: 200, duration: 3 },
-  { name: 'Honda', image: 'https://image.kilimall.com/kenya/shop/store/goods/7442/2021/07/7442_06801939004978952_720.jpg.webp', amount: 150, duration: 2 },
-  { name: 'Yamaha', image: 'https://image.kilimall.com/kenya/shop/store/goods/7442/2021/07/7442_06801939004978952_720.jpg.webp', amount: 180, duration: 2 },
-  { name: 'Kawasaki', image: 'https://image.kilimall.com/kenya/shop/store/goods/7442/2021/07/7442_06801939004978952_720.jpg.webp', amount: 170, duration: 2 },
-  { name: 'Suzuki', image: 'https://image.kilimall.com/kenya/shop/store/goods/7442/2021/07/7442_06801939004978952_720.jpg.webp', amount: 160, duration: 1 }
+  { name: 'Harley Davidson', image: 'https://image.kilimall.com/kenya/shop/store/goods/7442/2021/07/7442_06801939004978952_720.jpg.webp', amount: 200, duration: 3,description:"This is a superbike" },
+  { name: 'Honda', image: 'https://image.kilimall.com/kenya/shop/store/goods/7442/2021/07/7442_06801939004978952_720.jpg.webp', amount: 150, duration: 2,description: "This is a superbike"},
+  { name: 'Yamaha', image: 'https://image.kilimall.com/kenya/shop/store/goods/7442/2021/07/7442_06801939004978952_720.jpg.webp', amount: 180, duration: 2,description: "This is a superbike" },
+  { name: 'Kawasaki', image: 'https://image.kilimall.com/kenya/shop/store/goods/7442/2021/07/7442_06801939004978952_720.jpg.webp', amount: 170, duration: 2,description: "This is a superbike" },
+  { name: 'Suzuki', image: 'https://image.kilimall.com/kenya/shop/store/goods/7442/2021/07/7442_06801939004978952_720.jpg.webp', amount: 160, duration: 1,description: "This is a superbike" }
 ]
 motorcycles_data.each do |motorcycle_data|
   Motorcycle.create(motorcycle_data)
