3db6fcd13bcdc302e152a39d58eee6d926e4c746
Harshika Govind <harshika0102@gmail.com>
2023-08-31 16:29:29 +0530

    remove comments from file

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -1,16 +1,8 @@
-# FactoryBot.define do
-#   factory :reservation do
-#     user { nil }
-#     motorcycle { nil }
-#   end
-# end
-# spec/factories/reservations.rb
-# spec/factories/reservations.rb
 FactoryBot.define do
   factory :reservation do
     association :user
     association :motorcycle
-    date { Date.today + rand(1..30).days } # Generate a random future date
+    date { Date.today + rand(1..30).days }
     city { 'Sample City' }
     status { 'pending' }
   end
