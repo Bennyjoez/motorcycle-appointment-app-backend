187d4887c2a27892f51b2e16ba4a925f43cbf6d5
Harshika Govind <harshika0102@gmail.com>
2023-08-31 16:37:49 +0530

    Ensure unique usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   has_many :reservations
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
 end
