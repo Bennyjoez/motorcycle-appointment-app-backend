c67da6965b04a1fbc0d3562239ab21024f3ce17d
Harshika Govind <harshika0102@gmail.com>
2023-08-31 16:07:02 +0530

    add seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+motorcycles_data = [
+  { name: "Harley Davidson", image: "https://image.kilimall.com/kenya/shop/store/goods/7442/2021/07/7442_06801939004978952_720.jpg.webp", amount: 200, duration: 3 },
+  { name: "Honda", image: "https://image.kilimall.com/kenya/shop/store/goods/7442/2021/07/7442_06801939004978952_720.jpg.webp", amount: 150, duration: 2 },
+  { name: "Yamaha", image: "https://image.kilimall.com/kenya/shop/store/goods/7442/2021/07/7442_06801939004978952_720.jpg.webp", amount: 180, duration: 2 }
+]
